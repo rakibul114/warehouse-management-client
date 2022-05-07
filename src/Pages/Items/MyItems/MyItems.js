5296d33498a6c26340e3f9e169309dce487e508f
rakibul114 <rakibulislam114@gmail.com>
2022-05-07 23:44:10 +0600

    updated myitems

--- src/Pages/Items/MyItems/MyItems.js
+++ src/Pages/Items/MyItems/MyItems.js
@@ -5,25 +5,24 @@ import auth from '../../../firebase.init';
 import Item from '../Item/Item';
 
 const MyItems = () => {
-    const [user] = useAuthState(auth);    
+  const [user] = useAuthState(auth);
   const [items, setItems] = useState([]);
-  
+  useEffect(() => {
+
+    const getItems = async () => {
+      const email = user.email;
+      const url = `http://localhost:5000/item?email=${email}`;
+      const { data } = await axios.get(url);
+      setItems(data);
+    };
+    getItems();
     
-    useEffect(() => {
-      const getItems = async () => {
-        const email = user.email;
-        const url = `http://localhost:5000/item?email=${email}`;
-        const { data } = await axios.get(url);        
-        setItems(data);
-      };
-      getItems();
-    }, [user]);
+  }, [user]);
+    
     return (
         <div className='container'>
-        <h2 className='text-center my-4'>Your items</h2>
-        {
-          items.map(item => <Item key={item._id} item={item}></Item>)
-        }
+        <h2 className='text-center my-4'>Your items: {items.length }</h2>
+        
         </div>
     );
 };
